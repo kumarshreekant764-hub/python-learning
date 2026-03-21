0232df175da07b6a0c6f476fb1c198208e462b89
Shree <kumarshreekant764@gmail.com>
2026-03-21 22:59:34 +0530

    Condition= if,elif,else

---
+++ if,elif,else.py
@@ -0,0 +1,17 @@
+#vote eligibility
+age = 20
+if age >= 18:
+    print("You can vote!")
+elif age == 17:
+    print("Almost! One more year.")
+else:
+    print("Too young to vote.")
+
+# Checking marks
+marks = 40
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 60:
+    print("Grade: B")
+else:
+    print("Grade: C")
