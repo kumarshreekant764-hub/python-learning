e556174dcdec5ac61c987a41bff169470ba9fa25
Shree <kumarshreekant764@gmail.com>
2026-03-21 23:16:31 +0530

    loop code

---
+++ loop.py
@@ -0,0 +1,16 @@
+# for loop — count 1 to 34
+for i in range (1, 35):
+   # print(i)
+    print(f"Count: {i}")
+    
+    
+    # for loop — iterate over a list of fruits
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(f"I like {fruit}!")
+
+    # while loop
+count = 1
+while count <= 35:
+    print(f"Count: {count}")
+    count += 1
