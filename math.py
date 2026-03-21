cc522345df7c2c7707eeebe09a7ada339eccf6cd
Shree <kumarshreekant764@gmail.com>
2026-03-21 22:45:45 +0530

    math code

---
+++ math.py
@@ -0,0 +1,16 @@
+a = 10
+b = 3
+# Addition
+print("Addition:", a + b)
+# Subtraction
+print("Subtraction:", a - b)
+# Multiplication
+print("Multiplication:", a * b)
+# Division
+print("Division:", a / b)
+# Division (Floor)
+print("Division (Floor):", a // b)
+# Modulus
+print("Modulus:", a % b)
+# Exponentiation
+print("Exponentiation:", a ** b)
