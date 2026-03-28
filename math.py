d95d37793bf0257dc2be21fc2cefc2524dba159d
Shree <kumarshreekant764@gmail.com>
2026-03-29 00:31:17 +0530

    assignment operators program

--- math.py
+++ math.py
@@ -1,16 +1,15 @@
+#assignment operators
 a = 10
-b = 3
-# Addition
-print("Addition:", a + b)
-# Subtraction
-print("Subtraction:", a - b)
-# Multiplication
-print("Multiplication:", a * b)
-# Division
-print("Division:", a / b)
-# Division (Floor)
-print("Division (Floor):", a // b)
-# Modulus
-print("Modulus:", a % b)
-# Exponentiation
-print("Exponentiation:", a ** b)
+a += 5
+print(a)
+a -= 5
+print(a)
+a *= 5
+print(a)
+a /= 5
+print(a)
+a //= 5
+print(a)
+a **= 5
+print(a)
+
