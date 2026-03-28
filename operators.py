10041357fd131a4724c3e64fea1ddbc5927eba79
Shree <kumarshreekant764@gmail.com>
2026-03-29 00:44:58 +0530

    operators and its type program

---
+++ operators.py
@@ -0,0 +1,59 @@
+#Arithmetic operators
+a = 10
+b = 5
+print(a + b) #addition
+print(a - b) #subtraction
+print(a * b) #multiplication
+print(a / b) #division
+print(a // b) #floor division
+print(a % b) #modulus
+print(a ** b) #exponentiation
+
+
+#relational operators / comparison operators
+a = 10
+b = 5
+print(a == b) #equal to
+print(a != b) #not equal to
+print(a > b) #greater than
+print(a < b) #less than
+print(a >= b) #greater than or equal to
+print(a <= b) #less than or equal to
+
+
+
+#assignment operators
+a = 10
+a += 5
+print(a)
+a -= 5
+print(a)
+a *= 5
+print(a)
+a /= 5
+print(a)
+a //= 5
+print(a)
+a **= 5
+print(a)
+
+
+
+#logical operators
+a = True
+b = False
+print(a and b) #logical AND
+print(a or b) #logical OR
+print(not a) #logical NOT
+print(not b) #logical NOT
+
+#bitwise operators
+a = 10 # in binary: 1010
+b = 5  # in binary: 0101
+print(a & b) # bitwise AND
+print(a | b) # bitwise OR
+print(a ^ b) # bitwise XOR
+print(~a)    # bitwise NOT
+print(a << 1) # left shift
+print(a >> 1) # right shift
+
