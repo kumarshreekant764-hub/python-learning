9eb0583d6e62654d6c17c0f8bc0950955de996c5
Shree <kumarshreekant764@gmail.com>
2026-03-29 01:47:12 +0530

    parctices questions in python

---
+++ practices_questions.py
@@ -0,0 +1,28 @@
+# write a program to input two numbers and print their sum
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print("The sum is : ", sum)
+
+
+#wap to input side of square and print area of square
+side = int(input("enter side of square : "))
+area = side * side
+print("area of square is : " , area)
+
+
+#wap to input 2 floating point number & print their average
+num1 = float(input("enter first number : "))
+num2 = float(input("enter second number : "))
+average = (num1 + num2) / 2
+print("average is : " , average)
+
+
+
+#wap to input 2 int numbers' a and b print true if a is greater than or equal to b .if not print false
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+print(num1 >= num2)
+
+
